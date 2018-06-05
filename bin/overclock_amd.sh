0eb9819a0fc257db55dfdc04930a6e4e40a07149
coinscrow <info@minerstat.com>
2018-06-05 05:18:43 +0200

    VDDC for AMD undervolting
    Rewrite for my.minerstat.com

--- bin/overclock_amd.sh
+++ bin/overclock_amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-exec 2>/dev/null
+
 
 if [ ! $1 ]; then
 echo ""
@@ -9,6 +9,7 @@ echo "a = GPUID"
 echo "b = Memory Clock"
 echo "c = Core Clock"
 echo "d = Fan Speed"
+echo "e = VDDC"
 echo ""
 echo "-- Full Example --"
 echo "./overclock_amd 0 1750 1100 80 1.11"
@@ -24,22 +25,31 @@ VDDC=$5
 
 if [ "$CORECLOCK" != "skip" ]
 then
-sudo ./amdcovc coreclk:$GPUID=$CORECLOCK | grep "Setting core clock"
-sleep 0.5
-sudo ./amdcovc ccoreclk:$GPUID=$CORECLOCK | grep "Setting current core"
+	sudo ./amdcovc coreclk:$GPUID=$CORECLOCK | grep "Setting core clock"
+	sleep 0.5
+	sudo ./amdcovc ccoreclk:$GPUID=$CORECLOCK | grep "Setting current core"
 fi
 
 if [ "$MEMCLOCK" != "skip" ]
 then
-sudo ./amdcovc memclk:$GPUID=$MEMCLOCK | grep "Setting memory clock"
-sleep 0.5
-sudo ./amdcovc cmemclk:$GPUID=$MEMCLOCK | grep "Setting current memory"
+	sudo ./amdcovc memclk:$GPUID=$MEMCLOCK | grep "Setting memory clock"
+	sleep 0.5
+	sudo ./amdcovc cmemclk:$GPUID=$MEMCLOCK | grep "Setting current memory"
 fi
 
 if [ "$FANSPEED" != "skip" ]
 then
-sudo ./ohgodatool -i $GPUID --set-fanspeed $FANSPEED
-sudo ./amdcovc fanspeed:$GPUID=$FANSPEED | grep "Setting"
+	sudo ./ohgodatool -i $GPUID --set-fanspeed $FANSPEED
+	sudo ./amdcovc fanspeed:$GPUID=$FANSPEED | grep "Setting"
+fi
+
+if [ "$VDDC" != "skip" ]
+then
+	UNDERVOLT=$(sudo ./ohgodatool -i $GPUID --mem-state 3 --vddci $VDDC)
+	if echo "$UNDERVOLT" | grep "exist" ;then
+		echo "Trying to set VDDC for Memory state (2)";
+		sudo ./ohgodatool -i $GPUID --mem-state 2 --vddci $VDDC
+	fi
 fi
 
 sleep 2
