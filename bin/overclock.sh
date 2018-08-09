fcf650fe2d9b271242fcc7761c98c7dd399fbfe2
coinscrow <hello@coinscrow.io>
2018-08-09 20:33:22 +0200

    Update overclock.sh

--- bin/overclock.sh
+++ bin/overclock.sh
@@ -49,7 +49,7 @@ if [ ! -z "$DONVIDIA" ]; then
 	sync
 	
 	# NO IDEA, BUT THIS SOLVE P8 STATE ISSUES (ON ALL CARD!)
-	sudo nvidia-settings -a [gpu:0]/GPUPowerMizerMode=1	
+	sudo nvidia-settings -a [gpu:0]/GPUPowerMizerMode=1 -c :0	
 	sleep 0.5
 	
 	sudo chvt 1
