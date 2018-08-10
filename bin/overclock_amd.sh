78724d2c449bb4329db87ee2be7d8fea773e2d1b
coinscrow <info@minerstat.com>
2018-08-10 05:33:37 +0200

    AMD Overclock Improvements

--- bin/overclock_amd.sh
+++ bin/overclock_amd.sh
@@ -30,11 +30,7 @@ if [ $1 ]; then
 	# MDDC is not implemented due not effect much Polaris gpu's and..
 	# really stable around 1000mV 
 	MDDC=$7
-	
-	#################################£
-	# Overwrite PowerPlay to manual
-	echo manual > /sys/class/drm/card$GPUID/device/power_dpm_force_performance_level 
-	
+		
 	## BULIDING QUERIES
 	STR1="";
 	STR2="";
@@ -49,25 +45,10 @@ if [ $1 ]; then
 		STR2="fanspeed:$GPUID=$FANSPEED";
 	fi
 	
-	## Detect memory state's
-	MEMSTATES="2"
+	## Detect state's
+	MEMSTATES=$(sudo ./ohgodatool -i $GPUID --show-mem  | grep -E "Memory state ([0-9]+):" | tail -n 1 | sed -r 's/.*([0-9]+).*/\1/')
+	CORESTATE=$(sudo ./ohgodatool -i $GPUID --show-core | grep -E "DPM state ([0-9]+):"    | tail -n 1 | sed -r 's/.*([0-9]+).*/\1/')
 	
-	CHECKMEM=$(sudo ./ohgodatool -i $GPUID --show-mem)
-  	if echo "$CHECKMEM" | grep "Memory state 1:" ;then
-		MEMSTATES="1"
-	fi
-	
-	CHECKMEMA=$(sudo ./ohgodatool -i $GPUID --show-mem)
-  	if echo "$CHECKMEMA" | grep "Memory state 2:" ;then
-		MEMSTATES="2"
-	fi
-	
-	CHECKMEMB=$(sudo ./ohgodatool -i $GPUID --show-mem)
-  	if echo "$CHECKMEMB" | grep "Memory state 3:" ;then
-		MEMSTATES="3"
-	fi
-	
-	echo $MEMSTATES > /sys/class/drm/card$GPUID/device/pp_dpm_mclk
 	
 	echo "--- FOUND MEMORY STATE: $MEMSTATES ---"
 		
@@ -76,107 +57,90 @@ if [ $1 ]; then
 		if [ "$VDDC" != "0" ]  
 		then
 			# set all voltage states from 1 upwards to xxx mV:
-			for gpuid in $GPUID; do 
 			echo "--- Setting up VDDC Voltage GPU$gpuid ---"
-
-		if [ "$MEMSTATES" != "2" ]  
-		then
-			echo "MEMSTATE NOT Equals to 2";
-			for voltstate in 0 8 9 10; do  
-			sudo ./ohgodatool -i $gpuid --volt-state $voltstate --vddc-table-set $VDDC 
-			done
-		else
-			echo "MEMSTATE Equals to 2";
-			for voltstate in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do  
-			sudo ./ohgodatool -i $gpuid --volt-state $voltstate --vddc-table-set $VDDC 
-			done
-		fi
-			done
- 		 
-		 if [ "$VDDCI" != "" ]  
-		 then
-		 if [ "$VDDCI" != "0" ]  
-		 then
-		 if [ "$VDDCI" != "skip" ]  
-		 then
-			# VDDCI Voltages 
-			# VDDC Voltage + 50
-			for gpuid in $GPUID; do 
-			echo "--- Setting up VDDCI Voltage GPU$gpuid ---" 
-			for memstate in 1 2; do 
-				sudo ./ohgodatool -i $gpuid --mem-state $memstate --vddci $VDDCI  
-			done
-			done
+			
+			if [ "$MEMSTATES" != "2" ]  
+			then
+				for voltstate in 0 8 9 10; do  
+				sudo ./ohgodatool -i $GPUID --volt-state $voltstate --vddc-table-set $VDDC 
+				done
+			else
+				for voltstate in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do  
+				sudo ./ohgodatool -i $GPUID --volt-state $voltstate --vddc-table-set $VDDC 
+				done
 			fi
-		      fi
-		      else
-		      if [ "$VDDCI" != "skip" ]  
-		      then
-		      VDDCI=$(expr "$VDDC" + 50)
-		      for memstate in 1 2; do 
-		      	 	sudo ./ohgodatool -i $GPUID --mem-state $memstate --vddci $VDDCI  
-		      done
-		      fi
-		   fi
+			
 		fi
+	
+	
+	 if [ "$VDDCI" != "" ]  
+	 then
+	 if [ "$VDDCI" != "0" ]  
+	 then
+	 if [ "$VDDCI" != "skip" ]  
+	 then
+		# VDDCI Voltages 
+		# VDDC Voltage + 50
+			echo "--- Setting up VDDCI Voltage GPU$gpuid ---" 
+			sudo ./ohgodatool -i $GPUID --mem-state $MEMSTATES --vddci $VDDCI  
 	fi
-
-
+	fi
+	fi
+		   
+		
+	fi
+	
 #################################£
 # SET MEMORY @ CORE CLOCKS
 
-	if [ "$CORECLOCK" != "skip" ]
+	if [ "$CORECLOCK" != "skip" ] 
+	then	
+	if [ "$MEMCLOCK" != "skip" ] 
 	then
-		if [ "$MEMCLOCK" != "skip" ]
-		then
-			if [ "$CORECLOCK" != "0" ]
-			then
-				if [ "$MEMCLOCK" != "0" ]
-				then
+	if [ "$CORECLOCK" != "0" ] 
+	then
+	if [ "$MEMCLOCK" != "0" ] 
+	then
+	
 		# APPLY AT THE END
-		STR3="memclk:$GPUID=$MEMCLOCK"
 		STR4="cmemclk:$GPUID=$MEMCLOCK"
 		STR5="coreclk:$GPUID=$CORECLOCK"
-		STR6="ccoreclk:$GPUID=$CORECLOCK"
 		
 		# Set new clocks in tables
-		for gpuid in $GPUID; do 
 		echo "--- Setting up CoreStates and MemClocks GPU$gpuid ---"
-	if [ "$MEMSTATES" != "2" ]  
-	then
-		echo "-- MEMSTATE NOT Equals to 2 --";
-		echo 7 > /sys/class/drm/card$gpuid/device/pp_dpm_sclk
-		echo "- CORESTATE has been set to: 7 -"
-		for corestate in 7; do
-			sudo ./ohgodatool -i $gpuid --core-state $corestate --core-clock $CORECLOCK --mem-state $MEMSTATES --mem-clock $MEMCLOCK $STR1
-		done
-	else
-		echo "-- MEMSTATE Equals to 2 --";
-		echo 5 > /sys/class/drm/card$gpuid/device/pp_dpm_sclk
-	    echo "- CORESTATE has been set to: 5 -"
-		for corestate in 3 4 5 6 7; do
-			if [ "$corestate" != "7" ]
+		echo "-- MEMSTATE Equals to $MEMSTATES --";
+	  	echo "- CORESTATE has been set to: $CORESTATE -"
+	  		if [ "$MEMSTATES" != "2" ]  
 			then
-				sudo ./ohgodatool -i $gpuid --core-state $corestate --core-clock $CORECLOCK --mem-state $MEMSTATES --mem-clock $MEMCLOCK
+					sudo ./ohgodatool -i $GPUID --core-state $CORESTATE --core-clock $CORECLOCK --mem-state $MEMSTATES --mem-clock $MEMCLOCK $STR1
 			else
-				sudo ./ohgodatool -i $gpuid --core-state $corestate --core-clock $CORECLOCK --mem-state $MEMSTATES --mem-clock $MEMCLOCK $STR1
-
+				for corestate in 3 4 5 6 7; do
+				if [ "$corestate" != "$CORESTATE" ] 
+				then
+					sudo ./ohgodatool -i $GPUID --core-state $corestate --core-clock $CORECLOCK --mem-state $MEMSTATES --mem-clock $MEMCLOCK
+				else
+					sudo ./ohgodatool -i $GPUID --core-state $corestate --core-clock $CORECLOCK --mem-state $MEMSTATES --mem-clock $MEMCLOCK $STR1
+				fi
+				done
 			fi
-		done
-	fi
-		done
-
-		fi
-	fi
-	fi
-	fi
+	   
+	#################################£
+	# Overwrite PowerPlay to manual
+	sudo echo "manual" > /sys/class/drm/card$GPUID/device/power_dpm_force_performance_level 
+	sudo echo $CORESTATE > /sys/class/drm/card$GPUID/device/pp_dpm_sclk
+	sudo echo $MEMSTATES > /sys/class/drm/card$GPUID/device/pp_dpm_mclk 
+	
+	 fi
+	 fi
+	 fi
+	 fi
 	
 		#################################£
 		# Apply Changes
 		#sudo ./amdcovc memclk:$GPUID=$MEMCLOCK cmemclk:$GPUID=$MEMCLOCK coreclk:$GPUID=$CORECLOCK ccoreclk:$GPUID=$CORECLOCK $STR2 | grep "Setting"
-		sudo ./amdcovc $STR3 $STR4 $STR5 $STR6 $STR2 | grep "Setting"
+		sudo ./amdcovc $STR4 $STR5 $STR2 | grep "Setting"
 		
 		##################################
-		# Clock Protection
-		sudo ./amdcovc ccoreclk:5=$CORECLOCK memclk:5=$MEMCLOCK
+		# CURRENT_Clock Protection
+		sudo ./amdcovc ccoreclk:$GPUID=$CORECLOCK memclk:$GPUID=$MEMCLOCK | grep "Setting"
 fi
