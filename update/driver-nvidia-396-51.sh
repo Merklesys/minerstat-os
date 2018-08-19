bf81dd017baaabb60a49eed8e0c00595f2da1245
coinscrow <hello@coinscrow.io>
2018-08-19 21:23:12 +0200

    Create driver-nvidia-396-51.sh

---
+++ update/driver-nvidia-396-51.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo ""
+echo "=== UPDATING NVIDIA DRIVERS TO v396.51 ==="
+echo ""
+echo "--- Ctrl + C to abort it. ---"
+
+sleep 3
+
+sudo apt-get --yes --force-yes purge snapd ubuntu-core-launcher squashfs-tools
+sudo apt-get --yes --force-yes update
+sudo apt-get --yes --force-yes install cuda-runtime-9-2
+
+sleep 3
+
+echo ""
+
+echo "You need to reboot to apply changes."
+echo""
+
+echo "=== REBOOTING ==="
+echo "--- Ctrl + C to abort it. ---"
+
+sleep 5
+
+sudo shutdown -r now
