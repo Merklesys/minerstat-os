9991d87a1c0771b4315b14c76dd5b5bf14456808
coinscrow <info@minerstat.com>
2018-08-31 01:37:44 +0200

    restore changes

--- monitor.js
+++ monitor.js
@@ -1,4 +1,3 @@
-var monitorObject = {};
 module.exports = {
     /*
     	DETECT VIDEO CARD TYPE
@@ -34,9 +33,9 @@ module.exports = {
             query = exec(global.path + "/bin/amdcovc", function(error, stdout, stderr) {
                 var amdResponse = stdout,
                     queryPower = exec("cd " + global.path + "/bin/; sudo ./rocm-smi -P | grep 'GPU Power' | sed 's/.*://' | sed 's/W/''/g' | xargs", function(error, stdout, stderr) {
-                        //console.log("Estimated GPU Consumption(s): " + stdout);
-                        isfinished(amdResponse, "amd", gpuSyncDone, cpuSyncDone, stdout);
-                    });
+                    //console.log("Estimated GPU Consumption(s): " + stdout);
+                    isfinished(amdResponse, "amd", gpuSyncDone, cpuSyncDone, stdout);
+                });
             });
     },
     /*
@@ -44,43 +43,29 @@ module.exports = {
     */
     HWnvidia: function(gpuSyncDone, cpuSyncDone) {
         var lstart = -1;
-        response_start = -1,
+            response_start = -1,
             exec = require('child_process').exec;
-        var gpunum;
+        var gpunum,
+            hwg = [],
+            hwf = [];
         gpunum = exec("nvidia-smi --query-gpu=count --format=csv,noheader | tail -n1", function(error, stdout, stderr) {
             var response = stdout;
             while (lstart != (response - 1)) {
                 lstart++;
-                processNvidia(lstart, gpuSyncDone, cpuSyncDone, response_start, response)
+                var datar = "",
+                    q2 = exec("nvidia-smi -i " + lstart + " --query-gpu=name,temperature.gpu,fan.speed,power.draw --format=csv,noheader | tail -n1", function(error, stdout, stderr) {
+                        datar = stdout;
+                        // ADD DATA TO ARRAY
+                        hwg.push(datar);
+                        response_start++;
+                        if (response_start == (response - 1)) {
+                            isfinished(hwg, "nvidia", gpuSyncDone, cpuSyncDone, "");
+                        }
+                    });
             } // END WHILE
         }); // END FETCH
     } // END HWNvidia
 } // END MODULE.EXPORT
-function processNvidia(lstart, gpuSyncDone, cpuSyncDone, response_start, response) {
-    var idFix = lstart;
-    setTimeout(function() {
-        // SET Timeout for driver crash, if one GPU fails send zero data
-        monitorObject[idFix] = "NVIDIA #001, 0, 0%, 0 W\n";
-        response_start++;
-        if (response_start == (response - 1)) {
-            isfinished(monitorObject, "nvidia", gpuSyncDone, cpuSyncDone, "");
-        }
-    }, 3500);
-    var q2 = exec("nvidia-smi -i " + lstart + " --query-gpu=name,temperature.gpu,fan.speed,power.draw --format=csv,noheader | tail -n1", function(error, stdout, stderr) {
-        // New Key - Value
-        //console.log("GPU" + idFix + " - " + stdout);
-        monitorObject[idFix] = stdout;
-        //monitorObject[lstart].push(stdout);
-        response_start++;
-        if (response_start == (response - 1)) {
-            isfinished(monitorObject, "nvidia", gpuSyncDone, cpuSyncDone, "");
-        }
-    });
-    setTimeout(function() {
-        q2.kill();
-    }, 2500);
-}
-
 function isfinished(hwdatar, typ, gpuSyncDone, cpuSyncDone, powerResponse) {
     if (typ === "nvidia") {
         // ARRAY to JSON
@@ -96,8 +81,6 @@ function isfinished(hwdatar, typ, gpuSyncDone, cpuSyncDone, powerResponse) {
     /*
     	SEND DATA TO ENDPOINT
     */
-    // UNSET
-    monitorObject = {};
     var main = require('./start.js');
     main.callBackHardware(hwdatas, gpuSyncDone, cpuSyncDone, hwPower);
 }
