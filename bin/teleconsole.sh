95246896fa760cbf6d38e4c9f7e67d3527e14fd1
coinscrow <info@minerstat.com>
2018-10-04 00:44:49 +0200

    new logfile name

--- bin/teleconsole.sh
+++ bin/teleconsole.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #exec 2>/dev/null
 
-sudo echo -n "" > /home/minerstat/minerstat-os/bin/teleproxy.0
+sudo echo -n "" > /home/minerstat/minerstat-os/bin/screenlog.0
 
 RESTARTEVERY=3600
 CURRENTSYNC=0
@@ -9,7 +9,7 @@ CURRENTSYNC=0
 echo "*-*-* Teleconsole *-*-*"
 
 run() {
-screen -L -Logfile teleproxy.0 -A -m -d -S teleproxy ./teleproxy -f gravitational.com:80
+screen -L -A -m -d -S teleproxy ./teleproxy -f gravitational.com:80
 }
 
 
@@ -17,12 +17,12 @@ start()
 {
 	echo "-- Starting Teleconsole --"
 	cd /home/minerstat/minerstat-os/bin/
-	sudo echo -n "" > /home/minerstat/minerstat-os/bin/teleproxy.0
+	sudo echo -n "" > /home/minerstat/minerstat-os/bin/screenlog.0
 	run
 }
 
 forcekill() {
-	sudo echo -n "" > /home/minerstat/minerstat-os/bin/teleproxy.0
+	sudo echo -n "" > /home/minerstat/minerstat-os/bin/screenlog.0
 	sudo killall teleproxy
 	sleep 0.5
 	sudo killall teleproxy
@@ -40,27 +40,27 @@ check()
 		echo "Timeout, Restarting Teleconsole";
     	CURRENTSYNC=0
     	forcekill
-    	sudo echo -n "" > /home/minerstat/minerstat-os/bin/teleproxy.0
+    	sudo echo -n "" > /home/minerstat/minerstat-os/bin/screenlog.0
     	#start
 	fi
 	# Basic health check
-	SEARCH=$(cat teleproxy.0 | grep "SSH tunnel cannot be established" | wc -L)
+	SEARCH=$(cat screenlog.0 | grep "SSH tunnel cannot be established" | wc -L)
 	if [ "$SEARCH" -gt 0 ]; then
 		echo "Seems teleconsole crashed"
 		forcekill
-    	sudo echo -n "" > /home/minerstat/minerstat-os/bin/teleproxy.0
+    	sudo echo -n "" > /home/minerstat/minerstat-os/bin/screenlog.0
     	#start
 	fi
 	# Over
-	CRASH=$(cat teleproxy.0 | grep "You have ended your session" | wc -L)
+	CRASH=$(cat screenlog.0 | grep "You have ended your session" | wc -L)
 	if [ "$CRASH" -gt 0 ]; then
 		echo "Seems teleconsole crashed"
 		forcekill
-    	sudo echo -n "" > /home/minerstat/minerstat-os/bin/teleproxy.0
+    	sudo echo -n "" > /home/minerstat/minerstat-os/bin/screenlog.0
     	#start
 	fi
 	# File size empty
-	CHARS=$(cat teleproxy.0 | wc -L)
+	CHARS=$(cat screenlog.0 | wc -L)
 	if [ "$CHARS" -lt 70 ]; then
 	forcekill	
 	fi
@@ -75,7 +75,7 @@ forcekill
 while true
 do 
 	sleep 30
-	TELEID=$(cat /home/minerstat/minerstat-os/bin/teleproxy.0 | grep WebUI | rev | cut -d ' ' -f 1 | rev | xargs)
+	TELEID=$(cat /home/minerstat/minerstat-os/bin/screenlog.0 | grep WebUI | rev | cut -d ' ' -f 1 | rev | xargs)
 	echo "TeleID: "$TELEID
 	check
 done
