defadf0c304fe210955530f5a3c3c527a8482f7b
coinscrow <info@minerstat.com>
2018-10-03 22:25:53 +0200

    cmdproxy

--- bin/teleconsole.sh
+++ bin/teleconsole.sh
@@ -1,58 +1,81 @@
 #!/bin/sh
 #exec 2>/dev/null
 
+sudo echo -n "" > /home/minerstat/minerstat-os/bin/teleproxy.0
+
 RESTARTEVERY=3600
 CURRENTSYNC=0
 
 echo "*-*-* Teleconsole *-*-*"
 
-forcekill() {
-PID=$(pidof teleproxy)
-sudo kill -9 $PID
+run() {
+screen -L -Logfile teleproxy.0 -A -m -d -S teleproxy ./teleproxy -f gravitational.com:80
 }
 
-check()
-{
-	echo "-- Checking health on Teleconsole --"
-	CURRENTSYNC=$(($CURRENTSYNC + 35))
-	if [ "$CURRENTSYNC" -gt "$RESTARTEVERY" ]; then
-		echo "Timeout, Restarting Teleconsole";
-    	CURRENTSYNC=0
-    	#forcekill
-    	sudo killall teleproxy
-    	sudo rm /home/minerstat/minerstat-os/bin/teleproxy.0
-    	start
-	fi
-	# Basic health check
-	SEARCH=$(cat teleproxy.0 | grep "SSH tunnel cannot be established" | wc -L)
-	if [ "$SEARCH" -gt 0 ]; then
-		echo "Seems teleconsole crashed"
-		#forcekill
-		sudo killall teleproxy
-    	sudo rm /home/minerstat/minerstat-os/bin/teleproxy.0
-    	start
-	fi
-}
 
 start()
 {
 	echo "-- Starting Teleconsole --"
 	cd /home/minerstat/minerstat-os/bin/
-	sudo rm /home/minerstat/minerstat-os/bin/teleproxy.0
-	screen -L -Logfile teleproxy.0 -A -m -d -S teleproxy ./teleproxy -f gravitational.com:80
-	sleep 15
-	TELEID=$(cat teleproxy.0 | grep WebUI | rev | cut -d ' ' -f 1 | rev | xargs)
-	echo $TELEID
+	sudo echo -n "" > /home/minerstat/minerstat-os/bin/teleproxy.0
+	run
 }
+
+forcekill() {
+	sudo echo -n "" > /home/minerstat/minerstat-os/bin/teleproxy.0
+	sudo killall teleproxy
+	sleep 0.5
+	sudo killall teleproxy
+	sleep 0.8
+	sudo killall teleproxy
+	sleep 5
+	start
+}
+
+check()
+{
+	echo "-- Checking health on Teleconsole --"
+	CURRENTSYNC=$(($CURRENTSYNC + 30))
+	if [ "$CURRENTSYNC" -gt "$RESTARTEVERY" ]; then
+		echo "Timeout, Restarting Teleconsole";
+    	CURRENTSYNC=0
+    	forcekill
+    	sudo echo -n "" > /home/minerstat/minerstat-os/bin/teleproxy.0
+    	#start
+	fi
+	# Basic health check
+	SEARCH=$(cat teleproxy.0 | grep "SSH tunnel cannot be established" | wc -L)
+	if [ "$SEARCH" -gt 0 ]; then
+		echo "Seems teleconsole crashed"
+		forcekill
+    	sudo echo -n "" > /home/minerstat/minerstat-os/bin/teleproxy.0
+    	#start
+	fi
+	# Over
+	CRASH=$(cat teleproxy.0 | grep "You have ended your session" | wc -L)
+	if [ "$CRASH" -gt 0 ]; then
+		echo "Seems teleconsole crashed"
+		forcekill
+    	sudo echo -n "" > /home/minerstat/minerstat-os/bin/teleproxy.0
+    	#start
+	fi
+	# File size empty
+	CHARS=$(cat teleproxy.0 | wc -L)
+	if [ "$CHARS" -lt 70 ]; then
+	forcekill	
+	fi
+	
+}
+
+# Start with APP
+forcekill
+
 
 # Start loop
 while true
 do 
-	if screen -list | grep -q "teleproxy";
-	then
-		check
-	else
-   		start
-	fi
-	sleep 35
+	sleep 30
+	TELEID=$(cat /home/minerstat/minerstat-os/bin/teleproxy.0 | grep WebUI | rev | cut -d ' ' -f 1 | rev | xargs)
+	echo "TeleID: "$TELEID
+	check
 done
