d707cb33eec9d89d0cd58073d162d2b45828a416
coinscrow <hello@coinscrow.io>
2018-10-03 20:54:17 +0200

    Update jobs.sh

--- bin/jobs.sh
+++ bin/jobs.sh
@@ -38,4 +38,4 @@ export OpenCL_ROOT=/opt/amdgpu-pro/lib/x86_64-linux-gnu
 sudo sed -i s/"#FSCKFIX=no"/"FSCKFIX=yes"/ /etc/default/rcS
 # TELEPROXY
 # cd /home/minerstat/minerstat-os/bin
-screen -A -m -d -S telp sh teleconsole.sh
+#screen -A -m -d -S telp sh teleconsole.sh
