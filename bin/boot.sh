9496d35ebbb2a3c13eb394484ab6287486a6f9af
coinscrow <hello@coinscrow.io>
2018-11-18 16:01:50 +0100

    Hostname on first boot

--- bin/boot.sh
+++ bin/boot.sh
@@ -28,6 +28,11 @@ if ! screen -list | grep -q "dummy"; then
     #cd /home/minerstat/minerstat-os/bin
     #sudo sh overclock.sh
 
+    # Change hostname
+    WNAME=$(cat /media/storage/config.js | grep 'global.worker' | sed 's/global.worker =/"/g' | sed 's/"//g' | sed 's/;//g' | xargs)
+    sudo su -c "echo '$WNAME' > /etc/hostname"      
+    sudo hostname -F /etc/hostname
+
     echo " "
     echo "-------- CONFIGURE NETWORK ADAPTERS --------------"
     SSID=$(cat /media/storage/network.txt | grep 'WIFISSID="' | sed 's/WIFISSID="//g' | sed 's/"//g' | xargs | wc -L)
