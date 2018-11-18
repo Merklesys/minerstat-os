d7bf7216f22654e08d295eb81c86793e8b3f3d5a
coinscrow <hello@coinscrow.io>
2018-11-18 16:05:30 +0100

    Update work.sh

--- bin/work.sh
+++ bin/work.sh
@@ -34,9 +34,9 @@ if ! screen -list | grep -q "dummy"; then
     echo ""
 
     # Change hostname
-    WNAME=$(cat /media/storage/config.js | grep 'global.worker' | sed 's/global.worker =/"/g' | sed 's/"//g' | sed 's/;//g' | xargs)
-    sudo su -c "echo '$WNAME' > /etc/hostname"      
-    sudo hostname -F /etc/hostname
+    #WNAME=$(cat /media/storage/config.js | grep 'global.worker' | sed 's/global.worker =/"/g' | sed 's/"//g' | sed 's/;//g' | xargs)
+    #sudo su -c "echo '$WNAME' > /etc/hostname"      
+    #sudo hostname -F /etc/hostname
 
     echo " "
     echo "-------- CONFIGURE NETWORK ADAPTERS --------------"
